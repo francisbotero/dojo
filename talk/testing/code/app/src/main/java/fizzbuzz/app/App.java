30d50691d4b5deab1a9ab08e6bfbb8eb00ab16c2
Frankie Botero <francis.botero@gmail.com>
2023-07-13 13:47:03 -0400

    fixed code formatting

--- talk/testing/code/app/src/main/java/fizzbuzz/app/App.java
+++ talk/testing/code/app/src/main/java/fizzbuzz/app/App.java
@@ -19,8 +19,7 @@ public class App {
     public static String translate(int i) {
         if (i % 15 == 0) {
             return"fizzbuzz";
-        }
-        else if (i % 3 == 0) {
+        } else if (i % 3 == 0) {
             return "fizz";
         } else if (i % 5 == 0) {
             return "buzz";
